c91d5600b1d1d8efcb076c52f573348ee7b7be29
Hibi Miyazaki <hibi.miyazaki@gmail.com>
2021-06-30 11:39:46 +0900

    association

--- yaml2sql.rb
+++ yaml2sql.rb
@@ -21,7 +21,7 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-def fake(str)
+def fake(str, objects)
   if str.start_with?("Faker") || str.start_with?("Gimei")
     begin
       eval str
@@ -29,18 +29,26 @@ def fake(str)
       p e
       str
     end
-  elsif str =~ /\{UUID\}/
-    str.gsub "{UUID}", SecureRandom.uuid
+  elsif str =~ /\{\{UUID\}\}/
+    str.gsub "{{UUID}}", SecureRandom.uuid
+  elsif str =~ /\{\{([\w\.]+)\}\}/
+    c = $1.split('.')
+    begin
+      objects[c[0]][c[1]]
+    rescue => e
+      p e
+      str
+    end
   else
     str
   end
 end
 
-def yaml_to_sql(yaml)
-  yaml['data'].each do |t, records|
-    table = t
-
+def yaml_to_sql(yaml, objects)
+  yaml['data'].each do |table, records|
     records.each do |name, fields|
+      attributes = {}
+
       columns = []
       values = []
 
@@ -48,23 +56,30 @@ def yaml_to_sql(yaml)
         columns << c
 
         if val.is_a? String
-          v = fake(val)
+          v = fake(val, objects)
 
           if v.is_a? String
-            values << "'" + fake(v) + "'"
+            values << "'" + v + "'"
+            attributes[c] = v
           else
             values << v
+            attributes[c] = v
           end
         else
           values << val
+          attributes[c] = val
         end
       end
+
+      objects[name] = attributes
 
       yield "INSERT INTO #{table} (#{columns.join(', ')}) VALUES (#{values.join(', ')});"
     end
   end
 
 end
+
+objects = {}
 
 Dir.glob("#{options[:input]}/*.yaml") do |yaml_file|
   sql_file = yaml_file.sub(options[:input], options[:output])
@@ -73,7 +88,7 @@ Dir.glob("#{options[:input]}/*.yaml") do |yaml_file|
   yaml = YAML.load_file(yaml_file)
 
   File.open(sql_file, 'w') do |f|
-    yaml_to_sql(yaml) do |sql|
+    yaml_to_sql(yaml, objects) do |sql|
       f.puts sql
     end
   end
